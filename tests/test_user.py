f898212d1255e15cdbc15828c44487d12f753b71
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-23 14:07:48 +0300

    add users model test function in test user.py file

---
+++ tests/test_user.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import User
+
+class UserModelTest(unittest.TestCase):
+  
+    def setUp(self):
+        
+        self.new_user = User(password = 'motivation')
+
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verify(self):
+        self.assertTrue(self.new_user.verify_password('motivation'))
