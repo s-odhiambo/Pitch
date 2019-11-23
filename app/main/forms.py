bf2bd6554c4953ad03fb33b888f3168f31276cab
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-23 12:01:47 +0300

    add upvote form and downvote form in forms.py

---
+++ app/main/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+
+
+
+class PitchForm(FlaskForm):
+	title = StringField('Title', validators=[Required()])
+	description = TextAreaField("Pitch Your Idea",validators=[Required()])
+	category = RadioField('Label', choices=[('movies','movies'), ('music','music'), ('art','art'),('general','general')],validators=[Required()])
+	submit = SubmitField('Submit)')
+
+class CommentForm(FlaskForm):
+	description = TextAreaField('Add you comments here!',validators=[Required()])
+	submit = SubmitField('Add')
+
+
+
+class UpvoteForm(FlaskForm):
+	submit = SubmitField('Submit')
+
+
+class Downvote(FlaskForm):
+	submit = SubmitField('Submit')
