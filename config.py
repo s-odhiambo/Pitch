b778fe6294b869b9687f0f6be3337c1fe5208275
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-23 14:12:34 +0300

    add class config for configurations of the parent class

---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'PITCH'
+    SENDER_EMAIL = 'samuelangienda1998@gmail.com'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:tynashee452@localhost/pitchcave'
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
