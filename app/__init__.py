8e098a30f60ecff427ff517a0049ac84bcbeb7c7
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-23 13:41:28 +0300

    add def create app function that register blue print in our init.py file  for name configuration

--- app/__init__.py
+++ app/__init__.py
@@ -15,3 +15,29 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 simple = SimpleMDE()
 mail = Mail()
+
+
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+    # app.secret_key = 'bf941e91d40b203f980ee9022f393569'
+    app.secret_key = 'motivation'
+    simple.init_app(app)
+
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+
+    return app
